f0e94d11294cb3c19360b9b88c4d55c1cbf118b8
Xen0Xys <red57101@gmail.com>
2019-12-09 12:15:09 +0100

    Update game_engine.py

--- Void RPG V2/game_engine.py
+++ Void RPG V2/game_engine.py
@@ -1,13 +1,20 @@
 from tkinter import *
 
 class GraphicEngine(Tk):
-    def __init__(self):
+    def __init__(self, _option_list = None):
         super().__init__()
+        if _option_list == None:
+            self.options = self.loadGraphicEngineOptions()
+        else:
+            self.options = _option_list
+    def loadGraphicEngineOptions(self):
+        return {}
+        
 
 class GameEngine():
     def __init__(self):
         pass
-    def startGameEngines():
+    def startGameEngines(self):
         self.graphic_engine = GraphicEngine()
 
 
