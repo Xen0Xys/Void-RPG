5618822b17b68660bfdf789405fa2759fcab40ac
Xen0Xys <red57101@gmail.com>
2019-12-09 12:09:48 +0100

    Add game_engine.py

---
+++ Void RPG V2/game_engine.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class GraphicEngine(Tk):
+    def __init__(self):
+        super().__init__()
+
+class GameEngine():
+    def __init__(self):
+        pass
+    def startGameEngines():
+        self.graphic_engine = GraphicEngine()
+
+
+if __name__ == "__main__":
+    engine = GameEngine()
