baa50af757317671ed25b54d4c37d8b11ac75bac
Xen0Xys <red57101@gmail.com>
2019-12-09 18:54:42 +0100

    Add map placment to V2

--- Void RPG V2/game_engine.py
+++ Void RPG V2/game_engine.py
@@ -24,6 +24,7 @@ import PIL.ImageTk
 from tkinter.font import Font
 from tkinter import *
 import threading
+import time
 import json
 import os
 
@@ -69,21 +70,47 @@ class MenuMain():
             self.parent.onWindowClosing()
             pass
         if arg=="playOne":
-            self.parent.loadMap()
+            threading.Thread(target=self.parent.loadMap).start()
 
 class LoadingView():
-    def __init__(self, _parent):
+    """Display loading view"""
+    def __init__(self, _parent, _size_x, _size_y):
         self.parent = _parent
+        self.size_x = _size_x
+        self.size_y = _size_y
         self.resetUI()
+        self.setupUI()
     def resetUI(self):
         for i in self.parent.winfo_children():
             i.destroy()
     def setupUI(self):
-        pass
+        police = Font(family="Oldania ADF Std", size=20)
+        Label(self.parent, text = "Loading...", anchor=W, font=police).place(x=self.size_x / 2 - self.size_x / 20, y=self.size_y / 2)
 
+class GameView():
+    """Display game view"""
+    def __init__(self, _parent, _graphic_engine_options, _player_information, _textures_list, _pil_textures_list,  _map_picture):
+        self.parent = _parent
+        self.graphic_engine_options = _graphic_engine_options
+        self.map_picture = _map_picture
+        self.resetUI()
+        self.setupElements()
+    def resetUI(self):
+        for i in self.parent.winfo_children():
+            i.destroy()
+    def setupElements(self):
+        self.wallpaper_canvas = Canvas(self.parent, width=self.graphic_engine_options["x_window_size"], height=self.graphic_engine_options["y_window_size"], bg="#9a9a9a", highlightthickness=0)
+        self.wallpaper_canvas.place(x=0, y=0)
+        self.picture = self.wallpaper_canvas.create_image(-3000, -8000, image=self.map_picture, anchor=NW)
+        #threading.Thread(target=self.moveLoop, args=(-3000, -8000, )).start()
+    def moveLoop(self, x, y):
+        for i in range(500000):
+            time.sleep(.01)
+            self.wallpaper_canvas.coords(self.picture, x + i, y + i / 2)
 
 class GraphicEngine(Tk):
-    def __init__(self, _graphic_engine_options=None):#Initialize game engine
+    def __init__(self, _graphic_engine_options=None):
+        #Initialize game engine
         super().__init__()
         if _graphic_engine_options == None:
             self.options = self.loadGraphicEngineOptions()
@@ -96,6 +123,7 @@ class GraphicEngine(Tk):
         #Closing window event
         self.destroy()
     def showWindow(self):
+        self.geometry("{}x{}".format(self.options["x_window_size"], self.options["y_window_size"]))
         self.resizable(width=False, height=False)
         self.mainloop()
     def loadGraphicEngineOptions(self):
@@ -138,7 +166,8 @@ class GraphicEngine(Tk):
                         print(e)
         return textures, pil_textures
     def loadMap(self):
-        LoadingView(self)
+        #Load map PIL picture
+        LoadingView(self, self.options["x_window_size"], self.options["y_window_size"])
         map_name = "earth"
         self.pil_map = PIL.Image.new("RGB", (15000, 15000))
         if self.options["progressive_map_generation"] == False:
@@ -151,9 +180,11 @@ class GraphicEngine(Tk):
         self.map = PIL.ImageTk.PhotoImage(self.pil_map)
         self.displayMap()
     def loadAroundPlayer(self):
+        #Load map all around player
         pass
     def displayMap(self):
-        print("display_map")
+        #Display map on screen
+        GameView(self, self.options, None, self.textures, self.pil_textures, self.map)
 
 
 class GameEngine():
