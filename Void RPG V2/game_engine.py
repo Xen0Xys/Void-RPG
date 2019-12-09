f8e9f8041b3c17fea9d801de5c2e5add45ef23a7
Xen0Xys <red57101@gmail.com>
2019-12-09 13:38:07 +0100

    Update game_engine.py

--- Void RPG V2/game_engine.py
+++ Void RPG V2/game_engine.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import os
 import json
+import threading
 
 class GraphicEngine(Tk):
     def __init__(self, _graphic_engine_options=None):
@@ -13,21 +14,26 @@ class GraphicEngine(Tk):
         def createOptions():
             options = {}
             #Init all options here
+            options["x_window_size"] = 300
+            options["y_window_size"] = 300
             return options
         def loadOptions():
             with open("ressources/configuration/graphic_engine.json", "r") as file:
                 json_content = json.loads(file.read())
             return json_content
-        if os.path.exists("ressources/configuration/graphic_engine.json"):
+        if not os.path.exists("ressources/configuration/graphic_engine.json"):
             return createOptions()
         else:
             return loadOptions()
+    def loadingView(self):
+        self.geometry("{}x{}".format(self.options["x_window_size"], self.options["y_window_size"]))
 
 class GameEngine():
     def __init__(self):
-        pass
+        self.startGameEngines()
     def startGameEngines(self):
         self.graphic_engine = GraphicEngine()
+        self.graphic_engine.mainloop()
 
 
 if __name__ == "__main__":
