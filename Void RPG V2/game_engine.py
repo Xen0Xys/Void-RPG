004584183d673368a919f90edce1c95ae3c64053
Xen0Xys <red57101@gmail.com>
2019-12-09 15:07:00 +0100

    Upload new textures and update code

--- Void RPG V2/game_engine.py
+++ Void RPG V2/game_engine.py
@@ -4,13 +4,60 @@ import json
 import threading
 import time
 
+class EventListner():
+    def __init__(self):
+        pass
+
+
+class MenuMain():
+    def __init__(self, _parent, _ui_textures_list, _size_x, _size_y):
+        self.parent = _parent
+        self.ui_textures_list = _ui_textures_list
+        self.size_x = _size_x
+        self.size_y = _size_y
+        self.InitGUI()
+    def ResetGUI(self):
+        for i in self.parent.winfo_children():
+            i.destroy()
+    def InitGUI(self):
+        self.ResetGUI()
+        self.parent.title("Void - RPG")
+        self.SetGUI()
+        self.SetButtons()
+    def SetGUI(self):
+        self.MainCan = Canvas(self.parent, width=self.size_x, height=self.size_y, bg="#9a9a9a", highlightthickness=0)
+        self.MainCan.pack()
+    def SetButtons(self):
+        self.CanList=[]
+        Label(self.MainCan, text="Coding : Czekaj Tom\nGraphics : Duchene Guillaume, Choin Anatole", bg="#9a9a9a", justify="left").place(x=1,y=712)
+        self.CreateAllCan(600,75,75,50,self.ui_textures_list["baniere"], "", self.onClick)
+        self.CreateAllCan(50,50,610,680,self.ui_textures_list["option_wheel"], "option", self.onClick)
+        #self.CreateAllCan(220,75,30,600,self.ui_textures_list["fight"], "fight", self.onClick)
+        self.CreateAllCan(50,50,680,680,self.ui_textures_list["quit_button"], "quit", self.onClick)
+        self.CreateAllCan(220,75,30,300,self.ui_textures_list["create"], "playOne", self.onClick)
+        self.CreateAllCan(220,75,30,400,self.ui_textures_list["create"], "playTwo", self.onClick)
+        self.CreateAllCan(220,75,30,500,self.ui_textures_list["create"], "playThree", self.onClick)
+    def CreateAllCan(self, canwidth, canheight, x, y, image, arg, funct):
+        self.CanList.append(Canvas(self.MainCan, width=canwidth, height=canheight, bg="#9a9a9a", highlightthickness=0))
+        self.CanList[len(self.CanList)-1].place(x=x, y=y)
+        self.CanList[len(self.CanList)-1].create_image(0,0, image=image, anchor=NW)
+        self.CanList[len(self.CanList)-1].bind("<Button-1>", lambda arg1=None, arg2=arg:funct(arg1, arg2))
+    def onClick(self, evt, arg):
+        if arg=="quit":
+            #self.parent.onWindowClosing()
+            pass
+        if arg=="playOne":
+            pass
+
 class GraphicEngine(Tk):
     def __init__(self, _graphic_engine_options=None):
         super().__init__()
         if _graphic_engine_options == None:
-            self.loadGraphicEngineOptions()
+            self.options = self.loadGraphicEngineOptions()
         else:
             self.options = _graphic_engine_options
+        self.textures = self.loadTextures()
+        menu = MenuMain(self, self.textures["ui"], self.options["x_window_size"], self.options["y_window_size"])
     def showWindow(self):
         self.mainloop()
     def loadGraphicEngineOptions(self):
@@ -30,8 +77,19 @@ class GraphicEngine(Tk):
             return createOptions()
         else:
             return loadOptions()
-    def loadingView(self):
-        self.geometry("{}x{}".format(self.options["x_window_size"], self.options["y_window_size"]))
+    def loadTextures(self):
+        with open("ressources/configuration/textures.json", "r") as file:
+            json_content = json.loads(file.read())
+        textures = {}
+        for category in json_content.keys():
+            textures[category] = {}
+            for content in json_content[category].keys():
+                if json_content[category][content] != "NONE":
+                    try:
+                        textures[category][content] = PhotoImage(file="ressources/textures/{}/{}".format(category, json_content[category][content]))
+                    except TclError as e:
+                        print(e)
+        return textures
 
 class GameEngine():
     def __init__(self):
@@ -39,30 +97,6 @@ class GameEngine():
     def startGameEngines(self):
         self.graphic_engine = GraphicEngine()
         self.graphic_engine.showWindow()
-
-"""
-def algo():
-    with open("ressources/configuration/textures.cfg", "r") as file:
-        content = file.read()
-    ct_spt = content.split("\n")
-    next_ = []
-    for line in ct_spt:
-        try:
-            if line[0] != "#":
-                next_.append(line)
-        except IndexError:
-            pass
-    term = []
-    for line in next_:
-        term.append(line[:len(line) - 1])
-    dico = {}
-    dico["map"] = {}
-    for line in term:
-        s = line.split("=")
-        dico["map"][s[0]] = s[1]
-    with open("ressources/configuration/textures.json", "w") as file:
-        file.write(json.dumps(dico, indent=4))
-"""
 
 if __name__ == "__main__":
     engine = GameEngine()
