8f20b4f0d4aa372f3bef17fab436ececb38ee30c
Xen0Xys <red57101@gmail.com>
2019-12-09 16:00:20 +0100

    Update V2 code

--- Void RPG V2/game_engine.py
+++ Void RPG V2/game_engine.py
@@ -1,9 +1,30 @@
-from tkinter import *
+"""
+MIT License
+
+Copyright (c) [2019] [CZEKAJ Tom aka Xen0Xys, DUCHENE Guillaume aka Gunmar]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from PIL import Image, ImageTk
 from tkinter.font import Font
-import os
-import json
+from tkinter import *
 import threading
-import time
+import json
+import os
 
 class EventListner():
     def __init__(self):
@@ -15,49 +36,53 @@ class MenuMain():
         self.ui_textures_list = _ui_textures_list
         self.size_x = _size_x
         self.size_y = _size_y
-        self.InitGUI()
+        self.initUI()
     def resetGUI(self):
         for i in self.parent.winfo_children():
             i.destroy()
-    def InitGUI(self):
+    def initUI(self):
         self.resetGUI()
         self.parent.title("Void - RPG")
-        self.SetGUI()
-        self.SetButtons()
-    def SetGUI(self):
+        self.setUI()
+        self.setButtons()
+    def setUI(self):
         self.MainCan = Canvas(self.parent, width=self.size_x, height=self.size_y, bg="#9a9a9a", highlightthickness=0)
         self.MainCan.pack()
-    def SetButtons(self):
+    def setButtons(self):
         self.CanList=[]
         police = Font(family="Oldania ADF Std", size=10)
         Label(self.MainCan, text="Coding : Czekaj Tom, Duchêne Guillaume\nGraphics : Duchêne Guillaume, Choin Anatole", bg="#9a9a9a", justify="left", font=police).place(x=3,y=585)
-        self.CreateAllCan(600,75,12.5,50,self.ui_textures_list["baniere"], "", self.onClick)
-        self.CreateAllCan(50,50,495,560,self.ui_textures_list["option_wheel"], "option", self.onClick)
-        self.CreateAllCan(50,50,560,560,self.ui_textures_list["quit_button"], "quit", self.onClick)
-        self.CreateAllCan(220,75,30,200,self.ui_textures_list["create"], "playOne", self.onClick)
-        self.CreateAllCan(220,75,30,300,self.ui_textures_list["create"], "playTwo", self.onClick)
-        self.CreateAllCan(220,75,30,400,self.ui_textures_list["create"], "playThree", self.onClick)
-    def CreateAllCan(self, canwidth, canheight, x, y, image, arg, funct):
+        self.createCustomCanvas(600, 75, 12.5, 50, self.ui_textures_list["baniere"], "", self.onClick)
+        self.createCustomCanvas(50, 50, 495, 560, self.ui_textures_list["option_wheel"], "option", self.onClick)
+        self.createCustomCanvas(50, 50, 560, 560, self.ui_textures_list["quit_button"], "quit", self.onClick)
+        self.createCustomCanvas(220, 75, 30, 200, self.ui_textures_list["create"], "playOne", self.onClick)
+        self.createCustomCanvas(220, 75, 30, 300, self.ui_textures_list["create"], "playTwo", self.onClick)
+        self.createCustomCanvas(220, 75, 30, 400, self.ui_textures_list["create"], "playThree", self.onClick)
+    def createCustomCanvas(self, canwidth, canheight, x, y, image, arg, funct):
         self.CanList.append(Canvas(self.MainCan, width=canwidth, height=canheight, bg="#9a9a9a", highlightthickness=0))
         self.CanList[len(self.CanList)-1].place(x=x, y=y)
         self.CanList[len(self.CanList)-1].create_image(0,0, image=image, anchor=NW)
         self.CanList[len(self.CanList)-1].bind("<Button-1>", lambda arg1=None, arg2=arg:funct(arg1, arg2))
     def onClick(self, evt, arg):
         if arg=="quit":
-            #self.parent.onWindowClosing()
+            self.parent.onWindowClosing()
             pass
         if arg=="playOne":
-            pass
+            self.parent.loadMap()
 
 class GraphicEngine(Tk):
-    def __init__(self, _graphic_engine_options=None):
+    def __init__(self, _graphic_engine_options=None):#Initialize game engine
         super().__init__()
         if _graphic_engine_options == None:
             self.options = self.loadGraphicEngineOptions()
         else:
             self.options = _graphic_engine_options
         self.textures = self.loadTextures()
+        self.protocol("WM_DELETE_WINDOW", self.onWindowClosing)
         MenuMain(self, self.textures["ui"], self.options["x_window_size"], self.options["y_window_size"])
+    def onWindowClosing(self):
+        #Closing window event
+        self.destroy()
     def showWindow(self):
         self.resizable(width=False, height=False)
         self.mainloop()
@@ -69,6 +94,7 @@ class GraphicEngine(Tk):
             #Init all options here
             options["x_window_size"] = 625
             options["y_window_size"] = 625
+            options["progressive_map_generation"] = False
             with open("ressources/configuration/graphic_engine.json", "w") as file:
                 file.write(json.dumps(options, indent=4))
             return options
@@ -96,6 +122,11 @@ class GraphicEngine(Tk):
                     except TclError as e:
                         print(e)
         return textures
+    def loadMap(self):
+        if self.options["progressive_map_generation"] == True:
+            pass
+        else:
+            pass
 
 class GameEngine():
     def __init__(self):
