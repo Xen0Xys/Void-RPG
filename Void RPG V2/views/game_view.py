49c7750285a6af716aa940a098dc1b9df8319849
Xen0Xys <red57101@gmail.com>
2019-12-11 08:40:09 +0100

    Update V2 code

--- Void RPG V2/views/game_view.py
+++ Void RPG V2/views/game_view.py
@@ -1,4 +1,5 @@
 from tkinter.font import Font
+from player import Player
 from tkinter import *
 import threading
 import time
@@ -11,6 +12,11 @@ class GameView():
         self.map_picture = _map_picture
         self.resetUI()
         self.setupElements()
+        self.map_x, self.map_y = -3000, -8000
+        #Player setup
+        self.player = Player(self.map_x, self.map_y, self.picture)
+        self.parent.bind("<KeyPress>", lambda arg1=None, arg2="KeyPress":self.player.move(arg1, arg2))
+        self.parent.bind("<KeyRelease>", lambda arg1=None, arg2="KeyRelease":self.player.move(arg1, arg2))
     def resetUI(self):
         for i in self.parent.winfo_children():
             i.destroy()
@@ -18,8 +24,9 @@ class GameView():
         self.wallpaper_canvas = Canvas(self.parent, width=self.graphic_engine_options["x_window_size"], height=self.graphic_engine_options["y_window_size"], bg="#9a9a9a", highlightthickness=0)
         self.wallpaper_canvas.place(x=0, y=0)
         self.picture = self.wallpaper_canvas.create_image(-3000, -8000, image=self.map_picture, anchor=NW)
-        threading.Thread(target=self.moveLoop, args=(-3000, -8000, )).start()
+        #threading.Thread(target=self.moveLoop, args=(-3000, -8000, )).start()
     def moveLoop(self, x, y):
         for i in range(500000):
             time.sleep(.01)
             self.wallpaper_canvas.coords(self.picture, x + i, y + i / 2)
+    
