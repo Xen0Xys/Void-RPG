024f8780a29e04766f4452517270f9d2523fa030
gunmar06 <49071084+gunmar06@users.noreply.github.com>
2020-01-27 15:16:17 +0100

    Update player_stats_for_fight.py

--- Void RPG V2/fight_engine/player_stats_for_fight.py
+++ Void RPG V2/fight_engine/player_stats_for_fight.py
@@ -1,7 +1,18 @@
 
 class PlayerStatsForFight():
-    def __init__(self):
-        print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+    def __init__(self, itemObjectList, spellsObjectList):
+        self.Equipment={
+            "principal_hand":self.itemObjectList[1],
+            "secondary_hand":self.itemObjectList[0]
+            }
+        self.Spells_for_fight={
+            "first_spell":self.spellsObjectList[2],
+            "second_spell":None,
+            "third_spell":None,
+            "fourth_spell":None
+            }
+        print(Equipment["principal_hand"].name)
+        print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
         self.PV=100
         self.Speed=1
         self.Strength=10
@@ -11,3 +22,4 @@ class PlayerStatsForFight():
         self.Mana_Max=100
         self.defense=1.0
         self.statut="RAS"
+
