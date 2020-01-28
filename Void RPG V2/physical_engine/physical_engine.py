9023520b4c71c817841ed3f907eb3978defe5e3f
Xen0Xys <red57101@gmail.com>
2020-01-28 12:34:34 +0100

    Update DColLS

--- Void RPG V2/physical_engine/physical_engine.py
+++ Void RPG V2/physical_engine/physical_engine.py
@@ -10,6 +10,7 @@ class PhysicalEngine(threading.Thread):
         self.texture_size = self.chunck_loader.texture_size
         self.size = (self.graphic_engine.options["x_window_size"], self.graphic_engine.options["y_window_size"])
     def loadChunckCollisions(self, chunck):
+        chunck_matrix = chunck.matrix
         start_x = chunck.real_chunck_coords[0] // self.texture_size
         start_y = chunck.real_chunck_coords[1] // self.texture_size
         size_x = self.size[0]
@@ -20,10 +21,10 @@ class PhysicalEngine(threading.Thread):
     def listChunckWhoNeedLoadingCollisions(self):
         chunck_list = self.chunck_loader.chunck_list
         need_list = []
-        for chunck_y in range(len(chunck_list)):
-            for chunck_x in range(len(chunck_list[chunck_y])):
-                if chunck_list[chunck_y][chunck_x].collision_list == []:
-                    need_list.append(chunck_list[chunck_y][chunck_x])
+        for chunck_y in chunck_list:
+            for chunck_x in chunck_y:
+                if chunck_x.collision_list == []:
+                    need_list.append(chunck_x)
         return need_list
     def run(self):
         print("Running")
